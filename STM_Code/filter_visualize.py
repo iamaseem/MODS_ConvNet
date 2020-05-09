5210674715e87c3ac4ab40185367f85bb7ca041b
ASEEMMUHAMMAD A R <asaseemas@gmail.com>
2020-05-09 22:28:54 +0530

    Update filter_visualize.py

--- STM_Code/filter_visualize.py
+++ STM_Code/filter_visualize.py
@@ -99,7 +99,7 @@ def filter_viz():
 	    step = 1.
 
 	    # we start from a gray image with some random noise
-	    if K.image_dim_ordering() == 'th':
+	    if K.common.image_dim_ordering() == 'th':
 		input_img_data = np.random.random((1, 1, img_width, img_height))
 	    else:
 		input_img_data = np.random.random((1, img_width, img_height, 3))
